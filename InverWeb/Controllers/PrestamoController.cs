037b232075ce50c58fe173c19d9856076a3799c0
VismarRosario <vismar_rosario@hotmail.com>
2021-11-23 22:16:34 -0400

    Agregadas las vistas de prestamos

---
+++ InverWeb/Controllers/PrestamoController.cs
@@ -0,0 +1,29 @@
+﻿using InverWeb.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InverWeb.Controllers
+{
+    public class PrestamoController : Controller
+    {
+        // Crear una instancia del Contexto de la base de datos
+        private readonly GlobalDBContext _dbcontext;
+
+        public PrestamoController(GlobalDBContext dbcontext)
+        {
+            // Inicializar la base de datos
+            _dbcontext = dbcontext;
+        }
+
+        [HttpGet("admin/prestamos")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("admin/prestamo")]
+        public IActionResult Mostrar()
+        {
+            return View();
+        }
+    }
+}
